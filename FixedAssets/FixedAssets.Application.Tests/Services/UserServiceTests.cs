f9b24ba92354b21a954ce0008ca48a2a461c60b1
Luciana Rocha <lucianaregi@gmail.com>
2024-10-06 19:22:15 -0300

    test: adiciona projeto FixedAssets.Application.Tests e classes de teste - Adicionado novo projeto FixedAssets.Application.Tests à solução FixedAssets.sln para fins de teste.

---
+++ FixedAssets/FixedAssets.Application.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using FixedAssets.Application.Services;
+using FixedAssets.Domain.Entities;
+using FixedAssets.Infrastructure.Interfaces;
+
+namespace FixedAssets.Application.Tests.Services
+{
+    public class UserServiceTests
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IProductRepository> _productRepositoryMock; 
+        private readonly UserService _userService;
+
+        public UserServiceTests()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _productRepositoryMock = new Mock<IProductRepository>(); 
+
+            _userService = new UserService(_userRepositoryMock.Object, _productRepositoryMock.Object); 
+        }
+
+        [Fact]
+        public async Task GetUserById_ShouldReturnUser_WhenUserExists()
+        {
+            // Arrange
+            var userId = 1;
+            var expectedUser = new User { Id = userId, Name = "Test User", Balance = 1000 };
+
+            _userRepositoryMock.Setup(repo => repo.GetUserByIdAsync(userId))
+                .ReturnsAsync(expectedUser);
+
+            // Act
+            var user = await _userService.GetUserByIdAsync(userId);
+
+            // Assert
+            user.Should().BeEquivalentTo(expectedUser);
+        }
+
+        [Fact]
+        public async Task GetUserById_ShouldReturnNull_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userId = 1;
+            _userRepositoryMock.Setup(repo => repo.GetUserByIdAsync(userId))
+                .ReturnsAsync((User)null);
+
+            // Act
+            var user = await _userService.GetUserByIdAsync(userId);
+
+            // Assert
+            user.Should().BeNull();
+        }
+    }
+}
---
+++ FixedAssets/FixedAssets.Application.Tests/Validators/OrderValidatorTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixedAssets.Application.Tests.Validators
+{
+    internal class OrderValidatorTests
+    {
+    }
+}
